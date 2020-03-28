dd13fb569c0eef1970743a7c9ea2d0b4024a9b72
RuiHirano <r.hrn.0930@gmail.com>
2020-03-28 17:35:34 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Environment Variables in a Create React App on Netlify</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and commit to your repo.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p>NODE_ENV value is "{process.env.NODE_ENV}"</p>
+        <p>CUSTOM_ENV_VAR value is "{process.env.CUSTOM_ENV_VAR}"</p>
+        <p>REACT_APP_CUSTOM_ENV_VAR value is "{process.env.REACT_APP_CUSTOM_ENV_VAR}"</p>
+        <p>TOML_ENV_VAR value is "{process.env.TOML_ENV_VAR}"</p>
+        <p>REACT_APP_TOML_ENV_VAR value is "{process.env.REACT_APP_TOML_ENV_VAR}"</p>
+      </div>
+    );
+  }
 }
 
 export default App;
